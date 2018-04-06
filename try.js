562cbf89c13164d874eec35ae18d18d4bbc392f5
shikhi7 <mshikhar29@gmail.com>
2018-04-06 17:22:19 +0530

    Working get account data in try.js

--- try.js
+++ try.js
@@ -1,35 +1,17 @@
 const IOTA = require("iota.lib.js")
-const iota = new IOTA({provider: "http://localhost:14800"})
-//var sync = require('sync')
+// Create IOTA instance directly with provider
+var iota = new IOTA({
+    'provider': 'http://localhost:14800'
+});
 
-const fromseed = 'FRBOCU9PHRLCUYOGUCXXUHFPINKDVRJXBBHOHXXE9OTJMJKBBLYZNNNEXDCQYDSTRKIFYLRRX9WEDXKOC'
+// now you can start using all of the functions
+iota.api.getAccountData('QBIEJAQVOTT9CZLHXUZCRJPIWLOQKLYFCSUAMWFJUYAARWK9CFFXKSPYRPHJHF9BAPKWFOKOBAACIADXX', function(error, success) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(success);
+    }
+});
 
-let resp = ""
-iota.api.getNewAddress(fromseed, (error, success) => {
-	if (error) {
-		console.log(error)
-	}
-	else {
-		console.log(success)
-		resp = success
-	}
-})
-
-setTimeout(function(){console.log(resp);}, 500);
-
-// function x(){
-// 	var resp = "cummonnnn";
-// 	iota.api.getNewAddress(fromseed, (error, success) => {
-// 		if (error) {
-// 			console.log(error)
-// 		}
-// 		else {
-// 			console.log(success)
-// 			resp = success;
-// 		}
-// 	})
-// 	console.log(resp);
-// 	setTimeout(function(){return resp;}, 3000);
-// }
-
-// console.log(x())
+// you can also get the version
+console.log(iota.version);
